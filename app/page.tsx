5157094a96f319eda66a8f1c743eaf6bec36034c
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-20 15:07:50 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
     }
   }}
   placeholder="Type your message..."
-  rows={8}
+  rows={12}
   className="flex-1 px-4 py-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
   disabled={isLoading}
 />
